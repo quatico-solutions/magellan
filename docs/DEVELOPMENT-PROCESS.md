82b8c24dbd765144db10c58e7f8235241fbed4b4
Jan Wloka <jan.wloka@quatico.com>
2022-08-26 10:45:56 +0200

    c Adds missing license headers

--- docs/DEVELOPMENT-PROCESS.md
+++ docs/DEVELOPMENT-PROCESS.md
@@ -1,3 +1,9 @@
+<!--
+ ---------------------------------------------------------------------------------------------
+   Copyright (c) Quatico Solutions AG. All rights reserved.
+   Licensed under the MIT License. See LICENSE in the project root for license information.
+ ---------------------------------------------------------------------------------------------
+-->
 # Development process
 
 ## Creating a new release
--- packages/shared/src/transport/TransportHandler.ts
+++ packages/shared/src/transport/TransportHandler.ts
@@ -1,3 +1,9 @@
+/*
+ * ---------------------------------------------------------------------------------------------
+ *   Copyright (c) Quatico Solutions AG. All rights reserved.
+ *   Licensed under the MIT License. See LICENSE in the project root for license information.
+ * ---------------------------------------------------------------------------------------------
+ */
 import { TransportFunction } from "./TransportFunction";
 
 export type TransportHandler = {
